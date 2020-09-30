43e55ae13c161c96ee0798db589b2a29d175fb14
MiguelGuz <jose.guzman1343@alumnos.udg.mx>
2020-09-30 12:31:25 -0500

    se arreglo un ploblema con la inclusion de else if y retirada de todo los demas getline

--- main.cpp
+++ main.cpp
@@ -5,29 +5,33 @@ using namespace std;
 int main() {
     string cadena;
 
+    cout<<"cadena: "<<endl;
+    cin>>cadena;
+
     getline(cin, cadena);
-    if (cadena== "int"){
-        cout<<sizeof(int)<<endl;
-    }
-     getline(cin, cadena);
     if (cadena== "char"){
         cout<<sizeof(char)<<endl;
+        return 0;
     }
-     getline(cin, cadena);
-    if (cadena== "short"){
+     else if (cadena== "int"){
+        cout<<sizeof(int)<<endl;
+        return 0;
+    }
+    else if (cadena== "short"){
         cout<<sizeof(short)<<endl;
+        return 0;
     }
-     getline(cin, cadena);
-    if (cadena== "float"){
+    else if (cadena== "float"){
         cout<<sizeof(float)<<endl;
+        return 0;
     }
-     getline(cin, cadena);
-    if (cadena== "double"){
+    else if (cadena== "double"){
         cout<<sizeof(double)<<endl;
+        return 0;
     }
-     getline(cin, cadena);
-    if (cadena== "long"){
+    else if (cadena== "long"){
         cout<<sizeof(long)<<endl;
+        return 0;
     }
 
     return 0;
